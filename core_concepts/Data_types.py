0aae9cb82d2a311c45fbcceca874737389712f7a
Amulya komare <amulyakomare@gmail.com>
2026-05-05 20:39:38 +0530

    Add files via upload

---
+++ core_concepts/Data_types.py
@@ -0,0 +1,20 @@
+
+#Integer
+age = 18
+print(age)
+print(type(age))
+
+# Float
+percentage = 89.5
+print(percentage)
+print(type(percentage))
+
+# String
+name = "Amulya"
+print(name)
+print(type(name))
+
+# Boolean
+is_student = True
+print(is_student)
+print(type(is_student))
